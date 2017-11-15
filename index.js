ff85615757e96fe5d3848c20b215c6a4fbbc5fb4
Kazi Sanam <sanamish@gmail.com>
2017-11-15 04:07:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+    function scuberGreetingForFeet (distance){
+      if (distance <= 400){
+        return 'This one is on me!'
+      } else if (distance > 2000 && distance < 2500){
+        return 'I will gladly take your thirty bucks.'
+      } else if (distance> 2500){
+        return 'No can do.'
+      }
+    }
