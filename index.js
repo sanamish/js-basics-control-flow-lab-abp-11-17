a7093de626ebb47bade91c39c8ed4bc58f0cec68
Kazi Sanam <sanamish@gmail.com>
2017-11-15 04:32:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+    function scuberGreetingForFeet (distance){
+      if (distance <= 400){
+        return 'This one is on me!'
+      } else if (distance > 2000 && distance < 2500){
+        return 'I will gladly take your thirty bucks.'
+      } else if (distance> 2500){
+        return 'No can do.'
+      }
+    }
+function ternaryCheckCity(city) {
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  } else {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tipString){
+  if (tipString ==="generous"){
+    return "Thank you so much."
+  } else if (tipString ==="not as generous") {
+    return "Thank you."
+  } else{
+    return "Bye."
+  }
+}
